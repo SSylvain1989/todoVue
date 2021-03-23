ea6097d9e0bc05a60f317423a8303e70585dfa03
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-03-23 17:16:19 +0100

    edit tech ok

--- src/components/TechnoList.vue
+++ src/components/TechnoList.vue
@@ -2,11 +2,19 @@
 <h3>TechnoList</h3>
 {{technos.length}} techno{{technos.length > 1 ? "s" : ""}}
 <ul>
-  <li v-for="tech in technos" :key="tech.id"><button @click="deleteTechno(tech)">Supprimer</button> {{tech.techno}}</li>
+  <li v-for="tech in technos" :key="tech.id">
+    <button @click="editTechno(tech)">Editer</button>
+    <button @click="deleteTechno(tech)">Supprimer</button> 
+    <span v-if="technoToEdit !== null && technoToEdit.id === tech.id">
+      <input type="text" v-model="technoToEdit.techno"><button @click="save">sauvegarder</button>
+    </span>
+    <span v-else>{{tech.techno}}</span>
+    </li>
 </ul>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 export default {
   emits : ["delete-techno"],
   props: {
@@ -18,12 +26,23 @@ export default {
   },
   // au lieu de déclarer context puis faire context.emit on passe directement emit en destructure en deuxieme argument
   setup(props, {emit}){
+    let technoToEdit = ref(null);
+
     let deleteTechno = function(tech) {
       emit("delete-techno", tech);
 
+    };
+    let editTechno = function(tech) {
+      technoToEdit.value = tech;
+    };
+    let save = function() {
+      technoToEdit.value = null;
     }
     return{
       deleteTechno,
+      editTechno,
+      save,
+      technoToEdit,
     }
   }
 }
